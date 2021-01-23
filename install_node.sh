e53d74aa97e05091de40fb175173a33a8a16d2d8
Jose Luis Sanchez <muallin@gmail.com>
2021-01-23 17:29:23 +0100

    Adding npm

--- install_node.sh
+++ install_node.sh
@@ -2,6 +2,6 @@
 
 sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 apt-get update
-apt-get install -y --no-install-recommends nodejs node-gyp node-bindings
+apt-get install -y --no-install-recommends nodejs node-gyp node-bindings npm npx
 rm -rf /var/cache/apt/archives
 
