c2693edc778ae6eaabc3dbb6140b3ba7d22c8d87
Jose Luis Sanchez <muallin@gmail.com>
2021-01-23 17:30:14 +0100

    install nodem

--- install_node.sh
+++ install_node.sh
@@ -2,6 +2,7 @@
 
 sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 apt-get update
-apt-get install -y --no-install-recommends nodejs node-gyp node-bindings npm npx
+apt-get install -y --no-install-recommends nodejs node-gyp node-bindings npm
+npm install nodem
 rm -rf /var/cache/apt/archives
 
