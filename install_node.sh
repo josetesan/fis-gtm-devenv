90cf63b746204256d5a185add02e9494ec9d6626
Jose Luis Sanchez <muallin@gmail.com>
2021-01-23 17:37:30 +0100

    fix issue with npm registry

--- install_node.sh
+++ install_node.sh
@@ -3,6 +3,6 @@
 sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 apt-get update
 apt-get install -y --no-install-recommends nodejs node-gyp node-bindings npm
-npm install nodem
 rm -rf /var/cache/apt/archives
-
+npm config set registry http://registry.npmjs.org/  
+npm install nodem
